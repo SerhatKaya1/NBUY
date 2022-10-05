7c485c2472f12ded1f18384e3e5a48bfd84d6f50
Wizardkaya <Serhatkaya2496@gmail.com>
2022-10-05 18:15:12 +0300

    EN GÜNCE 5 EKİM

---
+++ Proje11_For/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Proje11_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i = 0; i < 5; i++)
+            //{ 
+            //  Console.WriteLine($"{i+1}.Hello, World!");
+            //}
+            // }
+
+            //Alt alta 11 kere 1 yazan program 
+            //int toplam = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    // topam = toplam + i;
+            //    toplam = toplam + 1;
+            //}
+            //Console.WriteLine($"Topam: {toplam}");
+
+
+            //1-10 Arasındaki çift sayıların toplamını buldurun.
+            //int toplam = 0;
+            //for (int i = 0; i <=10;  i=i+2)  //i++= i değerini 1 arttır
+            //    if (toplam == 0) ;
+            //{
+            //    toplam += i;     
+            //}
+            //Console.WriteLine(toplam);
+
+            //int toplam = 0;
+            //for (int i = 0; i <=10;  i++)
+            //{                            //i++= i değerini 1 arttır
+            //    if (i % 2 == 0) toplam += i;
+            //{
+            //        Console.WriteLine(toplam);
+            //    }
+
+            int ciftToplam = 0;
+            int tekToplam = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0) toplam += i;
+                {
+                    tekToplam += i;
+                }
+                else
+                {
+                    ciftToplam += i;
+
+                }
+
+
+           
+            }
+        }
+    }
+
+}
