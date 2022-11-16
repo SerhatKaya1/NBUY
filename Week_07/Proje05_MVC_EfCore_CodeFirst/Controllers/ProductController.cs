b2af356d112dd7f4decf3ecec8275941e4944b20
Wizardkaya <Serhatkaya2496@gmail.com>
2022-11-16 18:10:45 +0300

    ksjfs

---
+++ Week_07/Proje05_MVC_EfCore_CodeFirst/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Proje05_MVC_EfCore_CodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Proje05_MVC_EfCore_CodeFirst.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            MyDbContext context  = new MyDbContext(); /*Context üzerinden */
+        List<Product> products=context
+        .Products
+        .Include(p=> p.Category)
+        .ToList();
+            return View(products);
+        }
+    }
+}
---
+++ Week_07/Proje05_MVC_EfCore_CodeFirst/Migrations/20221116140142_InitalDb.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proje05_MVC_EfCore_CodeFirst.Migrations
+{
+    public partial class InitalDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Desc = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Citys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Citys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Desc = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Desc", "Name" },
+                values: new object[] { 1, "Phones", "Phone" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Desc", "Name" },
+                values: new object[] { 2, "Computers", "Computer" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Desc", "Name" },
+                values: new object[] { 3, "Electronics", "Electronic" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 34, "Istanbul" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 35, "Izmir" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 36, "Canakkale" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 1, 1, "Güzelbir telefon", "IPhone 13", 19000m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 2, 2, "Güzel PC", "Dell Xs", 29000m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 3, 1, "Güzel camera", "Samsung Xs", 39000m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 4, 3, "Güzel ses", "Piranha X13", 49000m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Citys");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
