992cebbb27298ff2d1870127f0493b842b3a2c14
Wizardkaya <Serhatkaya2496@gmail.com>
2022-11-18 18:01:26 +0300

    düzenleme

---
+++ Week_07/Proje06_ModelBinding_Form/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Proje06_ModelBinding_Form.Models;
+
+namespace Proje06_ModelBinding_Form.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult YasGrubu()
+    {
+        return View();
+    }
+    public IActionResult ad()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult YasGrubu(int yas, string ad)
+    {
+        return View();
+    }
+    if (yas<18)
+    {
+        ViewBag.YasGrubu="Reşit değilsiniz!";
+    }else if (yas<40)
+    {
+        ViewBag.YasGrubu="Gençsiniz!";
+    }else
+    {
+        //Burada veritabanı kayıtişlemi vb. yapılacak.
+         ViewBag.YasGrubu="Gençlere Taş Çıkartırsınız!";
+    }
+
+    //  public IActionResult VerileriAl(string txtAd, int txtYas)
+    // {
+    //     return View();
+    // }
+    
+}
+/*ctrl p + gene yazıp oradan ilkini seç ve cs.code klasörünü oluştur.*/
+/**/
---
+++ Week_07/Proje06_ModelBinding_Form/Views/Home/CreateProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje06_ModelBinding_Form.Views.Home
+{
+    public class CreateProduct.cshtml
+    {
+        
+    }
+}
